146032a9d65cac9e09b48d874010de5a6b668c0b
Rabin Gadal <gdlrabin123@gmail.com>
2025-02-18 22:08:48 +0545

    Signin route added incomplete

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,7 +17,7 @@ authRouter.post('/api/signup', async (req, res) => {
 
         // Hashing Password
         const hashedPassword = await bcryptjs.hash(password, 8);
-        
+
         let user = new User({
             name,
             email,
@@ -31,6 +31,27 @@ authRouter.post('/api/signup', async (req, res) => {
         res.status(500).json({ message: e.message });
     }
 
-})
+});
+
+// Sign In Route
+// Exercise
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res
+                .status(400)
+                .json({ message: 'User with this email does not exist!' });
+        }
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Incorrect password.' });
+        }
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+});
 
 module.exports = authRouter;
